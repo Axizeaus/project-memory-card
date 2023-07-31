5b4375d51906c8e686e6d8dab9be4728304e8d09
Axizeaus <axizeaus@gmail.com>
2023-07-31 19:18:54 +0630

    making it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import CardContainer from "./components/CardContainer";
 import Footer from "./components/Footer";
-import "./App.css";
+import "./App2.css";
 
 const App = () => {
   const [score, setScore] = useState(0);
